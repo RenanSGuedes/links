9f2903685c97eb744406c3165c814e544152bca7
Renan Guedes <55260736+RenanSGuedes@users.noreply.github.com>
2023-02-06 15:22:15 -0300

    :peer:

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ image = Image.open('./quasar.png')
 st.image(image)
 st.header("_Aplicações_")
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.subheader(":alien: :green[TWILIO]")
@@ -17,4 +17,10 @@ with col2:
     st.subheader(":space_invader: :blue[COMPILADOR CSV]")
     st.markdown(
         "[![Foo](https://i.imgur.com/wWAOXvS.png)](https://compilador-csv.streamlit.app/)"
+    )
+
+with col3:
+    st.subheader(":cow: :blue[CONTAGEM BHN]")
+    st.markdown(
+        "[![Foo](https://i.imgur.com/xmHRZ0h.png)](https://contagem.streamlit.app/)"
     )
