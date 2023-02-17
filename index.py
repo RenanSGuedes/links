daa4af381e1b224f1f743257e2da9851173e0823
Renan Guedes <55260736+RenanSGuedes@users.noreply.github.com>
2023-02-17 14:27:52 -0300

    :birds:

--- index.py
+++ index.py
@@ -9,18 +9,18 @@ st.header("_Aplicações_")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.subheader(":alien: :green[TWILIO]")
+    st.write(":alien: :green[TWILIO (WHATS)]")
     st.markdown(
         "[![Foo](https://i.imgur.com/xLUAqd6.png)](https://sheets-twilio.streamlit.app/)")
 
 with col2:
-    st.subheader(":space_invader: :blue[COMPILADOR CSV]")
+    st.write(":space_invader: :blue[COMPILADOR CSV (UPL)]")
     st.markdown(
         "[![Foo](https://i.imgur.com/wWAOXvS.png)](https://compilador-csv.streamlit.app/)"
     )
 
 with col3:
-    st.subheader(":cow: CONTAGEM BHN")
+    st.write(":cow: CONTAGEM (BHN)")
     st.markdown(
         "[![Foo](https://i.imgur.com/xmHRZ0h.png)](https://contagem.streamlit.app/)"
     )
